5c9159d2c167f313e783f1a0c85c4602b7280dae
Marc Morente <mmorente@inthinking.co.uk>
2023-01-12 12:13:04 +0100

    Move gpsIsAvailable, gpsIsNotAvailable functions to its own file

---
+++ utils/check-gps.js
@@ -0,0 +1,18 @@
+function gpsIsAvailable() {
+    return new Promise((resolve) => {
+        const availableGpsCoordinates = {
+            latitude: 1,
+            longitude: 1
+        };
+
+        resolve(availableGpsCoordinates);
+    });
+}
+
+function gpsIsNotAvailable() {
+    return new Promise((resolve, reject) => {
+        reject('GPS is not available');
+    });
+}
+
+export { gpsIsAvailable, gpsIsNotAvailable };
