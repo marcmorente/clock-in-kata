f24bba78916a2f69f31396bf79b2908fce013ebc
Marc Morente <mmorente@inthinking.co.uk>
2023-01-12 11:41:20 +0100

    Add mock-fetch.js

---
+++ utils/mock-fetch.js
@@ -0,0 +1,27 @@
+function mockFetchOk(time, gpsCoordinates) {
+    let warningMessage = typeof gpsCoordinates === 'undefined' ? 'Coordinates was not send' : '';
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                'status' : 'ok',
+                'warning' : warningMessage
+            });
+        }, 500);
+    });
+}
+
+function mockFetchError(time, gpsCoordinates) {
+    let warningMessage = typeof gpsCoordinates === 'undefined' ? 'Coordinates was not send' : '';
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject({
+                'status' : 'error',
+                'warning' : warningMessage
+            });
+        }, 500);
+    });
+}
+
+export { mockFetchOk, mockFetchError };
